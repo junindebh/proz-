8728a4511800318338a79c00782a61d341e9b9f7
junindebh <marcelojuniorbarbosa123@gmail.com>
2026-02-25 15:16:36 -0300

    atividade tema2

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let nome = prompt("Digite o seu nome:");
+let idade = prompt("Qual é a sua idade?");
+let cidade = prompt("Em qual cidade você mora?");
+
+console.log(`Usuário cadastrado: ${nome}, ${idade} anos, natural de ${cidade}.`);
+
+alert(`--- Ficha Cadastral ---
+Nome: ${nome}
+Idade: ${idade} 
+Cidade: ${cidade}`);
